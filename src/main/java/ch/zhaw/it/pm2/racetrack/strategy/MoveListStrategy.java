9297bf0fe6b430d62ba8207076a34e1e5290d87b
Shanke Wei (weisha01) <weisha01@students.zhaw.ch>
2026-03-15 09:58:11 +0100

    MoveListStrategy implemented has been added. A txt file containing the predefined moves has been added.

--- src/main/java/ch/zhaw/it/pm2/racetrack/strategy/MoveListStrategy.java
+++ src/main/java/ch/zhaw/it/pm2/racetrack/strategy/MoveListStrategy.java
@@ -3,11 +3,29 @@ package ch.zhaw.it.pm2.racetrack.strategy;
 import ch.zhaw.it.pm2.racetrack.game.Direction;
 
 import java.util.Optional;
+import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
 
 /**
  * Determines the next move based on a file containing a list of directions.
  */
 public class MoveListStrategy implements MoveStrategy {
+    List<String> moveList;
+
+    /**
+     * Instantiate the MoveListStrategy by reading the txt file containing all predefined moves.
+     */
+    public MoveListStrategy(){
+        try{
+            Path path = Paths.get("move_list.txt");
+            moveList = Files.readAllLines(path);
+        } catch (IOException e){
+            System.out.println("File not found");
+        }
+    }
 
     /**
      * {@inheritDoc}
@@ -17,6 +35,12 @@ public class MoveListStrategy implements MoveStrategy {
     @Override
     public Optional<Direction> nextMove() {
         // TODO: implementation
-        throw new UnsupportedOperationException();
+        if (!moveList.isEmpty()){
+            Direction currDirection = Direction.valueOf(moveList.get(0).toUpperCase());
+            moveList.remove(0);
+            return Optional.of(currDirection);
+        } else {
+            return Optional.empty();
+        }
     }
 }
