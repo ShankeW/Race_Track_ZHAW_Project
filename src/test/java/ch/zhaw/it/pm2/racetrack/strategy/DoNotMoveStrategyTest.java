2c8c02817dc24c522512f59508cd567c423bf10f
Hunter <hunterinswitzerland@gmail.com>
2026-03-18 16:35:22 +0100

    Created DoNotMoveStrategyTest

---
+++ src/test/java/ch/zhaw/it/pm2/racetrack/strategy/DoNotMoveStrategyTest.java
@@ -0,0 +1,11 @@
+package ch.zhaw.it.pm2.racetrack.strategy;
+
+import org.junit.jupiter.api.Test;
+
+public class DoNotMoveStrategyTest {
+
+    @Test
+    void testDoNotMoveStrategyCorrectVector(){
+        //TODO Create Test
+    }
+}
