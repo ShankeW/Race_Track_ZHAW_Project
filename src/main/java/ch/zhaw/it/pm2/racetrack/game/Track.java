4696b1c9a5dca9ed4a6971cb13217528e45698ee
Sridharan Ahalya (sridhaha) <sridhaha@students.zhaw.ch>
2026-03-25 15:50:39 +0100

    Add input validation and improve exception handling in Car and Track

--- src/main/java/ch/zhaw/it/pm2/racetrack/game/Track.java
+++ src/main/java/ch/zhaw/it/pm2/racetrack/game/Track.java
@@ -155,6 +155,9 @@ public class Track implements TrackSpecification {
      */
     @Override
     public Car getCar(int carIndex) {
+        if (carIndex < 0 || carIndex >= this.cars.size()) {
+            throw new IllegalArgumentException("carIndex out of bounds");
+        }
         return this.cars.get(carIndex);
     }
 
